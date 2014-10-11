fae4dc56c8a8825c753e76d4c3173772b23c5b3c
Ben <Ben>
2014-10-11 12:18:47 -0400

    updated test

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 import crib
 
+#this is a test method to make sure each card in deck is unique
 def allUnique(deck):
     seen = set()
     return not any(i in seen or seen.add(i) for i in deck)
 
+#create a deck of cards
 deck = crib.deck
-print allUnique(deck)
+
 #shuffled_deck = crib.shuffleDeck(deck)
 
 
 sDeck = crib.shuffleDeck(deck)
+print 'this is a complete deck of cards'
 print sDeck
-print allUnique(sDeck)
+print 'there are %s cards in the deck' % len(sDeck)
+print 'are the cards in the deck unique? %s ' % allUnique(sDeck)
 
